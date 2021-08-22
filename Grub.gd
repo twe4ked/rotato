8735ea44fcf7a268f672c4ee683949fd86687b39
Odin Dutton <odindutton@gmail.com>
2021-08-22 16:15:01 +1000

    Make grubs move

--- Grub.gd
+++ Grub.gd
@@ -2,12 +2,23 @@ extends KinematicBody2D
 
 var velocity = Vector2.ZERO
 var gravity = 10
+export var acceleration = 4
+export var max_speed = 10
+
+onready var wanderController = $WanderController
 
 func _ready():
 	$AnimatedSprite.play()
 
 func _physics_process(_delta):
+	if wanderController.get_time_left() == 0:
+		wanderController.start_timer(rand_range(1, 3))
+
+	var direction = global_position.direction_to(wanderController.target_position)
+	velocity = velocity.move_toward(direction * max_speed, acceleration)
+
 	velocity.y += gravity
+
 	velocity = move_and_slide(velocity, Vector2.UP)
 
 func _on_Hurtbox_area_entered(_area):
--- WanderController.gd
+++ WanderController.gd
@@ -1,12 +1,17 @@
 extends Node2D
 
 export var wander_range = 32
+export var horizontal_only = false
 
 onready var start_position = global_position
 onready var target_position = global_position
 
 func update_target_position():
-	var target_vector = Vector2(rand_range(-wander_range, wander_range), rand_range(-wander_range, wander_range))
+	var y = 0
+	if !horizontal_only:
+		y = rand_range(-wander_range, wander_range)
+
+	var target_vector = Vector2(rand_range(-wander_range, wander_range), y)
 	target_position = start_position + target_vector
 
 func get_time_left():
